af68f7fdfbb2cda6fb2afc072bbf26d00c51abe4
luetkemj <luetkemj@gmail.com>
2015-03-09 15:40:30 -0400

    Fix busted metabox

--- admin/class-benson-admin.php
+++ admin/class-benson-admin.php
@@ -115,17 +115,17 @@ class Benson_Admin {
 	 * Adds the meta box container.
 	 */
 	public function add_meta_box( $post_type ) {
-    $post_types = array('post', 'page');     //limit meta box to certain post types
-    if ( in_array( $post_type, $post_types )) {
-			add_meta_box(
-				'wpjson_url'
-				,__( 'Paste your wpjson url here.', 'myplugin_textdomain' )
-				,array( $this, 'render_meta_box_content' )
-				,$post_type
-				,'advanced'
-				,'high'
-			);
-    }
+            $post_types = array('post', 'page');     //limit meta box to certain post types
+            if ( in_array( $post_type, $post_types )) {
+		add_meta_box(
+			'benson_wp_json_url'
+			,__( 'Benson wp-json URL', 'benson_textdomain' )
+			,array( $this, 'render_meta_box_content' )
+			,$post_type
+			,'advanced'
+			,'high'
+		);
+            }
 	}
 
 	/**
@@ -141,13 +141,13 @@ class Benson_Admin {
 		 */
 
 		// Check if our nonce is set.
-		if ( ! isset( $_POST['myplugin_inner_custom_box_nonce'] ) )
+		if ( ! isset( $_POST['benson_inner_custom_box_nonce'] ) )
 			return $post_id;
 
-		$nonce = $_POST['myplugin_inner_custom_box_nonce'];
+		$nonce = $_POST['benson_inner_custom_box_nonce'];
 
 		// Verify that the nonce is valid.
-		if ( ! wp_verify_nonce( $nonce, 'myplugin_inner_custom_box' ) )
+		if ( ! wp_verify_nonce( $nonce, 'benson_inner_custom_box' ) )
 			return $post_id;
 
 		// If this is an autosave, our form has not been submitted,
@@ -170,7 +170,7 @@ class Benson_Admin {
 		/* OK, its safe for us to save the data now. */
 
 		// Sanitize the user input.
-		$mydata = sanitize_text_field( $_POST['myplugin_new_field'] );
+		$mydata = sanitize_text_field( $_POST['benson_new_field'] );
 
 		// Update the meta field.
 		update_post_meta( $post_id, 'wpjson_url', $mydata );
@@ -185,7 +185,7 @@ class Benson_Admin {
 	public function render_meta_box_content( $post ) {
 	
 		// Add an nonce field so we can check for it later.
-		wp_nonce_field( 'myplugin_inner_custom_box', 'myplugin_inner_custom_box_nonce' );
+		wp_nonce_field( 'benson_inner_custom_box', 'benson_inner_custom_box_nonce' );
 
 		// Use get_post_meta to retrieve an existing value from the database.
 		$value = get_post_meta( $post->ID, 'wpjson_url', true );
